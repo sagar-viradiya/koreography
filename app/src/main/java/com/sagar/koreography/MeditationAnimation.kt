2cc5b4f292c295f42a672dceda38ad28d7222bd5
Sagar Viradiya <sagarviradiya4444@gmail.com>
2023-06-07 17:56:38 +0200

    Code formatting
    - Add empty line at the end of MainActivity and MeditationAnimation
    - Remove unused imports from MeditationAnimation

--- app/src/main/java/com/sagar/koreography/MeditationAnimation.kt
+++ app/src/main/java/com/sagar/koreography/MeditationAnimation.kt
@@ -21,7 +21,6 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -29,12 +28,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import io.github.sagar_viradiya.LaunchKoreography
 import io.github.sagar_viradiya.rememberKoreography
 
 @Composable
