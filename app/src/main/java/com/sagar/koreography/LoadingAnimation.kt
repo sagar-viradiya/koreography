4af5e373201a3f9b46eb6d9711552c5347a31ee6
Sagar Viradiya <sagarviradiya4444@gmail.com>
2023-06-11 11:51:14 +0200

    Sample app enhancement (#13)
    - Add horizontal pager to show all animations
    - Put all animations within card

--- app/src/main/java/com/sagar/koreography/LoadingAnimation.kt
+++ app/src/main/java/com/sagar/koreography/LoadingAnimation.kt
@@ -20,14 +20,17 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.TransformOrigin
@@ -129,50 +132,49 @@ fun LoadingAnimation() {
         }
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Box(
-            modifier = Modifier
-                .width(208.dp)
-                .height(224.dp)
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.vector_1),
-                contentDescription = null,
-                modifier = Modifier
-                    .align(
-                        BiasAlignment(
-                            horizontalBias = rect1HorizontalAlignment.value,
-                            verticalBias = rect1VerticalAlignment.value
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+
+        Card(modifier = Modifier.fillMaxWidth().align(Alignment.Center), shape = RoundedCornerShape(16.dp),
+            elevation = 16.dp) {
+            Box(
+                modifier = Modifier.requiredWidth(232.dp).requiredHeight(240.dp).padding(16.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.vector_1),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(
+                            BiasAlignment(
+                                horizontalBias = rect1HorizontalAlignment.value,
+                                verticalBias = rect1VerticalAlignment.value
+                            )
                         )
-                    )
-                    .graphicsLayer {
-                        scaleX = scaleXRect1.value
-                        scaleY = scaleYRect1.value
-                        transformOrigin = TransformOrigin(0f, 0f)
-                    }
-            )
-            Image(
-                painter = painterResource(id = R.drawable.vector_2),
-                contentDescription = null,
-                modifier = Modifier
-                    .align(
-                        BiasAlignment(
-                            horizontalBias = rect2HorizontalAlignment.value,
-                            verticalBias = rect2VerticalAlignment.value
+                        .graphicsLayer {
+                            scaleX = scaleXRect1.value
+                            scaleY = scaleYRect1.value
+                            transformOrigin = TransformOrigin(0f, 0f)
+                        }
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.vector_2),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(
+                            BiasAlignment(
+                                horizontalBias = rect2HorizontalAlignment.value,
+                                verticalBias = rect2VerticalAlignment.value
+                            )
                         )
-                    )
-                    .graphicsLayer {
-                        scaleX = scaleXRect2.value
-                        scaleY = scaleYRect2.value
-                        transformOrigin = TransformOrigin(0f, 0f)
-                    }
-            )
+                        .graphicsLayer {
+                            scaleX = scaleXRect2.value
+                            scaleY = scaleYRect2.value
+                            transformOrigin = TransformOrigin(0f, 0f)
+                        }
+                )
+            }
         }
     }
+
 
     LaunchedEffect(true) {
         koreography.danceForever(this)
--- app/src/main/java/com/sagar/koreography/MeditationAnimation.kt
+++ app/src/main/java/com/sagar/koreography/MeditationAnimation.kt
@@ -23,7 +23,10 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -44,57 +47,90 @@ fun MeditationAnimation() {
     val scaleShadow = remember { Animatable(1f) }
     val koreography = rememberKoreography {
         parallelMoves {
-            move(animatable = offsetYMeditation, targetValue = -160f, animationSpec = infiniteRepeatable(animation = tween(1500), repeatMode = RepeatMode.Reverse) )
+            move(
+                animatable = offsetYMeditation,
+                targetValue = -160f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(1500),
+                    repeatMode = RepeatMode.Reverse
+                )
+            )
             move(
                 animatable = offsetYMeditationAura,
                 targetValue = -80f,
-                animationSpec = infiniteRepeatable(animation = tween(1500), repeatMode = RepeatMode.Reverse)
+                animationSpec = infiniteRepeatable(
+                    animation = tween(1500),
+                    repeatMode = RepeatMode.Reverse
+                )
             )
-            move(animatable = scaleAura, targetValue = 1.2f, animationSpec = infiniteRepeatable(animation = tween(1500), repeatMode = RepeatMode.Reverse))
-            move(animatable = scaleShadow, targetValue = 0.5f, animationSpec = infiniteRepeatable(animation = tween(1500), repeatMode = RepeatMode.Reverse))
+            move(
+                animatable = scaleAura,
+                targetValue = 1.2f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(1500),
+                    repeatMode = RepeatMode.Reverse
+                )
+            )
+            move(
+                animatable = scaleShadow,
+                targetValue = 0.5f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(1500),
+                    repeatMode = RepeatMode.Reverse
+                )
+            )
         }
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Image(
-            painter = painterResource(R.drawable.meditation_aura),
-            modifier = Modifier
-                .align(Alignment.Center)
-                .graphicsLayer {
-                    translationY = offsetYMeditationAura.value
-                    scaleX = scaleAura.value
-                    scaleY = scaleAura.value
-                },
-            contentDescription = null,
-            contentScale = ContentScale.Crop
-        )
 
-        Image(
-            painter = painterResource(R.drawable.meditation_shadow),
-            modifier = Modifier
-                .align(Alignment.Center)
-                .graphicsLayer {
-                    scaleX = scaleShadow.value
-                    transformOrigin = TransformOrigin(0.5f, 0.8f)
-                },
-            contentDescription = null,
-            contentScale = ContentScale.Crop
-        )
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp),
+            elevation = 16.dp
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(64.dp)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.meditation_aura),
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .graphicsLayer {
+                            translationY = offsetYMeditationAura.value
+                            scaleX = scaleAura.value
+                            scaleY = scaleAura.value
+                        },
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop
+                )
 
-        Image(
-            painter = painterResource(R.drawable.meditation),
-            modifier = Modifier
-                .align(Alignment.Center)
-                .graphicsLayer {
-                    translationY = offsetYMeditation.value
-                },
-            contentDescription = null,
-            contentScale = ContentScale.Crop
-        )
+                Image(
+                    painter = painterResource(R.drawable.meditation_shadow),
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .graphicsLayer {
+                            scaleX = scaleShadow.value
+                            transformOrigin = TransformOrigin(0.5f, 0.8f)
+                        },
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop
+                )
+
+                Image(
+                    painter = painterResource(R.drawable.meditation),
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .graphicsLayer {
+                            translationY = offsetYMeditation.value
+                        },
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
     }
 
     LaunchedEffect(true) {
